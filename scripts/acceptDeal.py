b8350c68800157cbcaada91b9f13e97ac54e1a9a
Volodymyr Sichka <vybgss@gmail.com>
2023-11-06 17:13:48 +0200

    Fix.

--- scripts/acceptDeal.py
+++ scripts/acceptDeal.py
@@ -13,7 +13,7 @@ from scripts.helpers import (
 def acceptDeal(asset, shopper,p2p):
     incresed_gas_price = get_increased_gas_price()
     p2p.acceptDeal(
-        p2p.address,
+        asset.address,
         {"from": shopper, "gas_price": incresed_gas_price}
     ).wait(1)
 
